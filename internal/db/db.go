8f17f2e5016eccce10f0bc7064f425905c518996
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-21 22:24:39 +0530

    added method to initialize database

---
+++ internal/db/db.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func InitDB(dataSourceName string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	// Verify connection
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
