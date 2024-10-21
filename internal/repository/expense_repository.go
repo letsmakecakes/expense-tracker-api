b16083743252ed9d69bde52977cc5d6d9a3bf7b3
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-21 22:45:45 +0530

    added method to return a repository object

--- internal/repository/expense_repository.go
+++ internal/repository/expense_repository.go
@@ -1,10 +1,21 @@
 package repository
 
-import "expensetrackerapi/pkg/models"
+import (
+	"database/sql"
+	"expensetrackerapi/pkg/models"
+)
 
 type ExpenseRepository interface {
 	Add(expense *models.Expense) error
 	Load() ([]*models.Expense, error)
 	Update(expense *models.Expense) error
 	Delete(id int) error
+}
+
+type expenseRepository struct {
+	db *sql.DB
+}
+
+func NewExpenseRepository(db *sql.DB) ExpenseRepository {
+	return &expenseRepository{db}
 }
