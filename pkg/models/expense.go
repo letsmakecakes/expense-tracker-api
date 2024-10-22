ac0aabfd1348f3062f8f0ca16b0938fcf7ec734b
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-22 19:24:11 +0530

    added changes to expense data model

--- pkg/models/expense.go
+++ pkg/models/expense.go
@@ -1,8 +1,12 @@
 package models
 
+import "time"
+
 type Expense struct {
-	ID          int    `json:"id"`
-	Date        string `json:"date"`
-	Description string `json:"description"`
-	Amount      int    `json:"amount"`
+	ID          int       `json:"id"`
+	Date        string    `json:"date"`
+	Description string    `json:"description"`
+	Amount      int       `json:"amount"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
