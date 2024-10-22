85bc2c59afd63ae342b907f0564a97776b5ed8a2
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-22 21:53:38 +0530

    Added method to update expense

--- internal/controllers/expense_controller.go
+++ internal/controllers/expense_controller.go
@@ -69,3 +69,46 @@ func (c *ExpenseController) LoadAllExpenses(ctx *gin.Context) {
 
 	utils.RespondWithJSON(ctx, http.StatusOK, expenses)
 }
+
+func (c *ExpenseController) UpdateExpense(ctx *gin.Context) {
+	idParam := ctx.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		log.Errorf("error updating expense: %v", err)
+		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid expense ID")
+		return
+	}
+
+	var expense models.Expense
+	if err := ctx.ShouldBindJSON(&expense); err != nil {
+		log.Errorf("error binding JSON: %v", err)
+		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	if err := utils.ValidateExpense(&expense); err != nil {
+		log.Errorf("error validating expense: %v", err)
+		utils.RespondWithError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	expense.ID = id
+	if err := c.Service.UpdateExpense(&expense); err != nil {
+		log.Errorf("error updating expense: %v", err)
+		if err == sql.ErrNoRows {
+			utils.RespondWithError(ctx, http.StatusNotFound, "Expense not found")
+		} else {
+			utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to update expense")
+		}
+		return
+	}
+
+	updatedExpense, err := c.Service.GetExpenseByID(id)
+	if err != nil {
+		log.Errorf("error getting expense: %v", err)
+		utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to retrieve updated expense")
+		return
+	}
+
+	utils.RespondWithJSON(ctx, http.StatusOK, updatedExpense)
+}
