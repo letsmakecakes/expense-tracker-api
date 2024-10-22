734c9160fec92d2fad688eec1373e0159724b9dc
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-22 21:43:46 +0530

    Added method to get expense by id

--- internal/controllers/expense_controller.go
+++ internal/controllers/expense_controller.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
+	"database/sql"
 	"expensetrackerapi/internal/services"
 	"expensetrackerapi/pkg/models"
 	"expensetrackerapi/pkg/utils"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
@@ -32,4 +34,27 @@ func (c *ExpenseController) AddExpense(ctx *gin.Context) {
 	}
 
 	utils.RespondWithJSON(ctx, http.StatusCreated, expense)
+}
+
+func (c *ExpenseController) GetExpense(ctx *gin.Context) {
+	idParam := ctx.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		log.Errorf("error converting paramater to number: %v", err)
+		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid exepnse ID")
+		return
+	}
+
+	expense, err := c.Service.GetExpenseByID(id)
+	if err != nil {
+		log.Errorf("error getting blog: %v", err)
+		if err == sql.ErrNoRows {
+			utils.RespondWithError(ctx, http.StatusNotFound, "Expense not found")
+		} else {
+			utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to retrieve expense")
+		}
+		return
+	}
+
+	utils.RespondWithJSON(ctx, http.StatusOK, expense)
 }
