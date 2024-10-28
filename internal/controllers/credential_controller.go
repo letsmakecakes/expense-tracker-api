7088e6140fa7f233e36f6f352fb21dc178b5bedd
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-28 21:43:33 +0530

    Added method to create a credential

--- internal/controllers/credential_controller.go
+++ internal/controllers/credential_controller.go
@@ -1,6 +1,14 @@
 package controllers
 
-import "expensetrackerapi/internal/services"
+import (
+	"expensetrackerapi/internal/services"
+	"expensetrackerapi/pkg/models"
+	"expensetrackerapi/pkg/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
 
 type CredentialController struct {
 	Service services.CredentialService
@@ -10,3 +18,36 @@ func NewCredentialController(service services.CredentialService) *CredentialCont
 	return &CredentialController{service}
 }
 
+// CreateCredential handles POST /signup
+func (c *CredentialController) CreateCredential(ctx *gin.Context) {
+	var credential models.Credential
+	if err := ctx.ShouldBindJSON(&credential); err != nil {
+		log.Errorf("error binding JSON: %v", err)
+		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	// Validate the credential
+	if err := utils.ValidateCredential(&credential); err != nil {
+		log.Errorf("error validating blog: %v", err)
+		utils.RespondWithError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	hashed_password, err := utils.HashPassword(credential.Password)
+	if err != nil {
+		log.Errorf("error hashing password: %v", err)
+		utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to hash password")
+		return
+	}
+
+	credential.Password = hashed_password
+
+	if err := c.Service.CreateCredential(&credential); err != nil {
+		log.Errorf("error creating credential: %v", err)
+		utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to create credential")
+		return
+	}
+
+	utils.RespondWithJSON(ctx, http.StatusCreated, credential)
+}
