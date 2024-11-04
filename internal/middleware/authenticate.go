60bb375873693d5d0d53535ea9942ba5220f734d
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-11-05 00:04:08 +0530

    Added middleware to check token is valid or not

---
+++ internal/middleware/authenticate.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"expensetrackerapi/pkg/jwt"
+	"expensetrackerapi/pkg/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Middleware to authenticate JWT tokens
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Retrieve the JWT token from the cookie
+		tokenString, err := c.Cookie("token")
+		if err != nil {
+			utils.RespondWithError(c, http.StatusUnauthorized, "Authorization token not found")
+			c.Abort()
+			return
+		}
+
+		// Parse and validate the token
+		claims, err := jwt.ValidateToken(tokenString)
+		if err != nil {
+			utils.RespondWithError(c, http.StatusUnauthorized, "Invalid or expired token")
+			c.Abort()
+			return
+		}
+
+		// Store user info in context for later use
+		c.Set("username", claims.Username)
+		c.Next()
+	}
+}
