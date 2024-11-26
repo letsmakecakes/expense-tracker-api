cbaa2bc52bd45b93326472b1e977e391a375dedb
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-11-26 10:52:22 +0530

    Added changes in error message format.

--- pkg/utils/validation.go
+++ pkg/utils/validation.go
@@ -8,13 +8,13 @@ import (
 
 func ValidateExpense(expense *models.Expense) error {
 	if strings.TrimSpace(expense.Date) == "" {
-		return errors.New("Date is required")
+		return errors.New("date is required")
 	}
 	if strings.TrimSpace(expense.Description) == "" {
-		return errors.New("Description is required")
+		return errors.New("description is required")
 	}
 	if expense.Amount == 0 {
-		return errors.New("Amount is required")
+		return errors.New("amount is required")
 	}
 
 	return nil
@@ -22,10 +22,10 @@ func ValidateExpense(expense *models.Expense) error {
 
 func ValidateCredential(credential *models.Credential) error {
 	if strings.TrimSpace(credential.Username) == "" {
-		return errors.New("Username is required")
+		return errors.New("username is required")
 	}
 	if strings.TrimSpace(credential.Password) == "" {
-		return errors.New("Password is required")
+		return errors.New("password is required")
 	}
 
 	return nil
