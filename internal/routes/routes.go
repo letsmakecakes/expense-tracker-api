5ba84faaf63e981935ba67d3f1c7219e79788524
letsmakecakes <adwaithrajeev@gmail.com>
2024-11-29 11:33:44 +0530

    Added changes in routing URI

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -11,6 +11,10 @@ import (
 )
 
 func SetupRoutes(router *gin.Engine, db *sql.DB) {
+	// Disable redirection for trailing slashes and fixed paths
+	router.RedirectTrailingSlash = false
+	router.RedirectFixedPath = false
+
 	// Initialize repositories, services, and controllers
 	expenseRepo := repository.NewExpenseRepository(db)
 	expenseService := services.NewExpenseService(expenseRepo)
@@ -28,13 +32,18 @@ func SetupRoutes(router *gin.Engine, db *sql.DB) {
 	expenseAPI := router.Group("/expenseAPI")
 	expenseAPI.Use(middleware.AuthMiddleware()) // Apply the AuthMiddleware here
 	{
-		expense := expenseAPI.Group("/expense")
+		// expense := expenseAPI.Group("/expense")
 		{
-			expense.POST("/", expenseController.AddExpense)         // Add an expense
-			expense.GET("/", expenseController.LoadAllExpenses)     // List all expenses
-			expense.GET("/:id", expenseController.GetExpense)       // Get a specific expense by ID
-			expense.PUT("/:id", expenseController.UpdateExpense)    // Update an expense
-			expense.DELETE("/:id", expenseController.DeleteExpense) // Delete an expense
+			// expense.POST("/", expenseController.AddExpense)                    // Add an expense
+			expenseAPI.POST("/expense", expenseController.AddExpense) // Handles /expenseAPI/expense
+			// expense.GET("/", expenseController.LoadAllExpenses)                // List all expenses
+			expenseAPI.GET("/expense", expenseController.LoadAllExpenses) // Handles /expenseAPI/expense
+			// expense.GET("/:id", expenseController.GetExpense)                  // Get a specific expense by ID
+			expenseAPI.GET("expense/:id", expenseController.GetExpense) // Handles /expenseAPI/expense/:id
+			// expense.PUT("/:id", expenseController.UpdateExpense)               // Update an expense
+			expenseAPI.PUT("/expense/:id", expenseController.UpdateExpense) // Handles /expenseAPI/expense/:id
+			// expense.DELETE("/:id", expenseController.DeleteExpense)            // Delete an expense
+			expenseAPI.DELETE("/expense/:id", expenseController.DeleteExpense) // Handles /expenseAPI/expense/:id
 		}
 	}
 }
